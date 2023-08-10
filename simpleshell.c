10ce86bd95b2d3668d0498dc74959f041fc527a1
Gharddammez <guavgharddammez@gmail.com>
2023-08-10 12:49:35 +0300

    Add beautiful code example

--- simpleshell.c
+++ simpleshell.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program.
+ * It prints a simple greeting message to the console.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+    /* Print a greeting message to the console */
     printf("Hello, Betty!\n");
+
+    /* Return 0 to indicate successful completion */
     return (0);
 }
 
